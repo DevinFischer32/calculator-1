4ec6122cc8f97ed2a7ea1f84a2b75fcabdd86d08
Devin Fischer <devinfischer32@gmail.com>
2021-07-26 15:44:35 -0600

    Finished Calculator

--- calc.js
+++ calc.js
@@ -14,8 +14,45 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+if (mathSymbol === '+'){
+	console.log(num1 + num2);
+}
 
-	// This line closes the connection to the command line interface.
+
+if (mathSymbol === '-'){
+	console.log(num1 - num2);
+}
+
+
+if (mathSymbol === '*'){
+	console.log(num1 * num2);
+}
+
+
+if (mathSymbol === '/'){
+	console.log(num1 / num2);
+}
+
+
+if (mathSymbol === 'squareRoot'){
+	console.log(Math.sqrt(num1) );
+}
+	
+if (mathSymbol === '^'){
+	console.log(Math.pow(num1 , num2) );
+}
+
+if (mathSymbol === '%'){
+	console.log(num1 % num2);
+}
+
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1)	
+	console.log('num2', num2)
+	
+	
+	
+	// This line closes the connection to the command line interfac.e
 	reader.close()
-
 });
+
